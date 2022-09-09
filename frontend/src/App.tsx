6187d92ffa89584be8113b1df336e041b41feb4d
Warley Dias <warleydias@gmail.com>
2022-09-08 21:29:44 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
-  return(
-    <h1>Olá Mundo!</h1>
-  )
+  return (
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
