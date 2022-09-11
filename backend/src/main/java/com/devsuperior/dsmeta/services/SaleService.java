b5661acbfcc838d298724a0a343d42eea8332f9b
Warley Dias <warleydias@gmail.com>
2022-09-11 10:38:33 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -14,17 +14,17 @@ import com.devsuperior.dsmeta.repositories.SaleRepository;
 
 @Service
 public class SaleService {
-		
-		@Autowired
-		private SaleRepository repository;
 
-		public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
-			
-			LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-			
-			LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
-			LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
-			
-			return repository.findSales(min, max, pageable);
-		}
+	@Autowired
+	private SaleRepository repository;
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+		return repository.findSales(min, max, pageable);
+	}
 }
