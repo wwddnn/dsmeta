54ac2692b33a534d730c0a0ccef16bd6e00678d1
Warley Dias <warleydias@gmail.com>
2022-09-11 20:30:53 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import NotificationButton from "./components/NotificationButton";
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
