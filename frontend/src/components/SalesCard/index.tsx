d81c10e9d77443fe87d72491aec74af47f43908e
Warley Dias <warleydias@gmail.com>
2022-09-11 20:03:15 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -17,11 +17,17 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`)
-    .then(response => {
+
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    console.log(dmin);
+
+    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+      .then(response => {
         setSales(response.data.content);
-    });
-  }, []);
+      });
+  }, [minDate, maxDate]);
 
   return (
     <div className="dsmeta-card">
@@ -60,7 +66,7 @@ function SalesCard() {
           </thead>
 
           <tbody>
-            {sales.map(sale => {
+            {sales.map((sale) => {
               return (
                 <tr key={sale.id}>
                   <td className="show992">{sale.id}</td>
